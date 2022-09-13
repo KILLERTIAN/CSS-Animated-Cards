8d312f82d9eb97099589ba332b3d789f5d32a575
Om Sharma <77867638+KILLERTIAN@users.noreply.github.com>
2022-09-13 21:08:30 +0530

    Added Multi Color changing Bar

--- script.js
+++ script.js
@@ -4,16 +4,57 @@ const bluebg = document.querySelector('.blue');
 const redbg = document.querySelector('.red');
 const yellowbg = document.querySelector('.yellow');
 const bodycolor = document.querySelector('.body');
+const cardshadow = document.getElementsByClassName('card');
 
 whitebg.addEventListener('click', () => {
     bodycolor.className = 'body ';
+    // cardshadow.className = 'card ';
     bodycolor.classList.toggle('--white-bg');
-    blackbg.classList.remove('--black-bg');
-    console.log(bodycolor.classList);
-    
+    // cardshadow.classList.toggle('--white-sh'); 
+    for (var i = 0; i < cardshadow.length; i++) {
+        cardshadow[i].className = 'card ';
+    } 
+    for (var i = 0; i < cardshadow.length; i++) {
+        cardshadow[i].classList.toggle('--white-sh');
+    }
 })
 blackbg.addEventListener('click', () => {
     bodycolor.className = 'body ';
-    bodycolor.classList.toggle('--black-bg')
-    // whitebg.classList.remove('--white-bg');
+    bodycolor.classList.toggle('--black-bg');
+    for (var i = 0; i < cardshadow.length; i++) {
+        cardshadow[i].className = 'card ';
+    }
+    for (var i = 0; i < cardshadow.length; i++) {
+        cardshadow[i].classList.toggle('--black-sh');
+    }
+})
+bluebg.addEventListener('click', () => {
+    bodycolor.className = 'body ';
+    bodycolor.classList.toggle('--blue-bg');
+    for (var i = 0; i < cardshadow.length; i++) {
+        cardshadow[i].className = 'card ';
+    }
+    for (var i = 0; i < cardshadow.length; i++) {
+        cardshadow[i].classList.toggle('--blue-sh');
+    }
+})
+redbg.addEventListener('click', () => {
+    bodycolor.className = 'body ';
+    bodycolor.classList.toggle('--red-bg');
+    for (var i = 0; i < cardshadow.length; i++) {
+        cardshadow[i].className = 'card ';
+    }
+    for (var i = 0; i < cardshadow.length; i++) {
+        cardshadow[i].classList.toggle('--red-sh');
+    }
+})
+yellowbg.addEventListener('click', () => {
+    bodycolor.className = 'body ';
+    bodycolor.classList.toggle('--yellow-bg');
+    for (var i = 0; i < cardshadow.length; i++) {
+        cardshadow[i].className = 'card ';
+    }
+    for (var i = 0; i < cardshadow.length; i++) {
+        cardshadow[i].classList.toggle('--yellow-sh');
+    }
 })
